e1be482efcf6d168972003b54f695f8d2ef175c2
Tamás Kovács <kovacs.tamas0408@gmail.com>
2022-09-30 16:33:53 +0200

    Add new document button

--- src/Components/docs.js
+++ src/Components/docs.js
@@ -4,6 +4,10 @@ const Docs = () => {
   return (
     <div className='docs_main'>
       <h1>Docs</h1>
+
+      <button className="add_doc_btn">
+        New document
+      </button>
     </div>
   );
 }
