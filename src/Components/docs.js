b16ad0cad06774caaaad32857673f1b682379028
Tamás Kovács <kovacs.tamas0408@gmail.com>
2022-09-30 18:53:24 +0200

    Update component

--- src/Components/docs.js
+++ src/Components/docs.js
@@ -1,22 +1,38 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import ModalComp from "./modalComp";
+import { addDoc, collection } from "firebase/firestore";
 
-const Docs = ({db}) => {
+const Docs = ({ db }) => {
   const [open, setOpen] = useState(false);
-  const [title, setTitle] = useState('');
+  const [title, setTitle] = useState("");
   const handleOpen = () => setOpen(true);
-  const addData = () => {}
+  const collectionRef = (db, "docsData");
+  const addData = () => {
+    addDoc(collectionRef, { title: title })
+      .then(() => {
+        alert("Data added!");
+      })
+      .catch(() => {
+        alert("Data cannot be added!");
+      });
+  };
 
   return (
-    <div className='docs_main'>
+    <div className="docs_main">
       <h1>Docs</h1>
 
       <button className="add_doc_btn" onClick={handleOpen}>
         ADD DOCUMENT
       </button>
-      <ModalComp open={open} setOpen={setOpen} title={title} setTitle={setTitle} addData={addData}/>
+      <ModalComp
+        open={open}
+        setOpen={setOpen}
+        title={title}
+        setTitle={setTitle}
+        addData={addData}
+      />
     </div>
   );
-}
+};
 
 export default Docs;
