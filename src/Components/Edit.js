570af21dc4d345f81142b39d60286a1144f54199
Tamás Kovács <kovacs.tamas0408@gmail.com>
2022-10-01 15:50:25 +0200

    Update

--- src/Components/Edit.js
+++ src/Components/Edit.js
@@ -1,27 +1,36 @@
-import React, {useState, useEffect} from 'react';
-import { useParams } from 'react-router-dom';
-import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.snow.css';
-import {updateDoc, collection, doc, onSnapshot} from 'firebase/firestore';
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import { updateDoc, collection, doc, onSnapshot } from "firebase/firestore";
 
-export default function Edit({db}) {
-  const [typing, setTyping] = useState('');
-  const collectionRef = collection(db, 'docsData')
+export default function Edit({ db }) {
+  const [typing, setTyping] = useState("");
+  const collectionRef = collection(db, "docsData");
   let params = useParams();
   const getQuillData = (value) => {
-    setTyping(value)
-  }
+    setTyping(value);
+  };
   useEffect(() => {
     const updateDocument = setTimeout(() => {
-
-    }, 1000)
+      const document = doc(collectionRef, params.id);
+      updateDoc(document, {
+        typing: typing,
+      })
+        .then(() => {
+          alert("Document saved!");
+        })
+        .catch(() => {
+          alert("Document cannot be saved!");
+        });
+    }, 1000);
     return () => clearTimeout(updateDocument);
-  }, [])
+  }, []);
 
   return (
     <div>
       <h1>Edit</h1>
-      <ReactQuill value={typing} onChange={getQuillData}/>
+      <ReactQuill value={typing} onChange={getQuillData} />
     </div>
-  )
+  );
 }
