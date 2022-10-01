1c495a3873b1c54e6f8b12bf2095f0dbc9f06d8c
Tamás Kovács <kovacs.tamas0408@gmail.com>
2022-10-01 15:23:07 +0200

    Set up Edit component

--- src/Components/Edit.js
+++ src/Components/Edit.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { useParams } from 'react-router-dom';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 
 export default function Edit({db}) {
+  const [typing, setTyping] = useState('');
   let params = useParams();
+  const getQuillData = (value) => {
+    setTyping(value)
+  }
 
   return (
     <div>
       <h1>Edit</h1>
-      <ReactQuill />
+      <ReactQuill value={typing} onChange={getQuillData}/>
     </div>
   )
 }
