37713a1f4c929acdf9cabafff63d17bfe07a82d7
Tamás Kovács <kovacs.tamas0408@gmail.com>
2022-10-01 15:37:43 +0200

    Update

--- src/Components/Edit.js
+++ src/Components/Edit.js
@@ -1,14 +1,22 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { useParams } from 'react-router-dom';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
+import {updateDoc, collection, doc, onSnapshot} from 'firebase/firestore';
 
 export default function Edit({db}) {
   const [typing, setTyping] = useState('');
+  const collectionRef = collection(db, 'docsData')
   let params = useParams();
   const getQuillData = (value) => {
     setTyping(value)
   }
+  useEffect(() => {
+    const updateDocument = setTimeout(() => {
+
+    }, 1000)
+    return () => clearTimeout(updateDocument);
+  }, [])
 
   return (
     <div>
