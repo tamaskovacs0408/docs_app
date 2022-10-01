d5f7cf7fe59412aed9ad8077c50c61c8cdb53f3c
Tamás Kovács <kovacs.tamas0408@gmail.com>
2022-10-01 12:34:30 +0200

    Set up routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import Docs from './Components/docs';
+import EditDocs from './Components/editDocs';
 import {app, db} from "./Firebase/config";
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <Docs db={db}/>
+    <Routes>
+      <Route path='/' element={<Docs db={db}/>}/>
+      <Route pat='/editDocs/:id' element={<EditDocs db={db}/>}/>
+    </Routes>
   );
 }
 
