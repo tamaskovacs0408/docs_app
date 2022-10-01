86884e9b821a5f4679bdc58168221f89f1001074
Tamás Kovács <kovacs.tamas0408@gmail.com>
2022-10-01 15:14:13 +0200

    Update component

--- src/Components/Edit.js
+++ src/Components/Edit.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 
 export default function Edit({db}) {
+  let params = useParams();
+
   return (
     <div>
-      <h2>EDIT</h2>
+      <h1>Edit</h1>
+      <ReactQuill />
     </div>
   )
 }
